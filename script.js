0a2ff939d74a1dde9d463aa5028877b5b72de56b
Student-Ayanpal <ayanp0759@gmail.com>
2026-04-21 23:19:01 +0530

    Updated files

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
 const API_KEY = "AIzaSyC_UMp1JaCu9APPqKTfQGJXtXM3y_3f5NA";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`;
 
 const userData = {
   message: null,
